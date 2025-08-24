6940918bb5c0fa43757deacd2bb504ef5e8ba93b
GaraevIM <garaevildarm@gmail.com>
2025-08-24 21:05:49 +0500

    update

---
+++ app/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id ("application")
+    kotlin("jvm") version "2.0.0" // если используешь Kotlin
+    id("com.github.ben-manes.versions") version "0.51.0"
+}
+application {
+    mainClass = "hexlet.code.App"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,11 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static String getUserName() {
+        System.out.print("May I have your name? ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+}
