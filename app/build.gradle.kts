8f7486d850d66bcb376428bd671db31aa6bc68d5
Ильдар <https://github.com/araev/java-project-61>
2025-09-03 16:42:39 +0500

    Обновлен build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,13 +2,21 @@ plugins {
     id ("application")
     kotlin("jvm") version "2.0.0" // если используешь Kotlin
     id("com.github.ben-manes.versions") version "0.51.0"
+    id ("checkstyle")
 }
+
 application {
     mainClass = "hexlet.code.App"
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+configure<CheckstyleExtension> {
+    toolVersion = "10.12.4"
+    configFile = file("config/checkstyle/checkstyle.xml")
+    isIgnoreFailures = false
+}
 
 repositories {
     mavenCentral()
@@ -20,14 +28,21 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-tasks.getByName("run", JavaExec::class) {
-    standardInput = System.`in`
-}
-
 tasks.test {
     useJUnitPlatform()
 }
 
 kotlin {
     jvmToolchain(21)
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
+tasks.withType<Checkstyle>().configureEach {
+    reports {
+        xml.required.set(false)
+        html.required.set(true)
+    }
 }
