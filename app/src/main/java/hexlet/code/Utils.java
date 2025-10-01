a25355553d89b8f3030bc7b4caad1ab6ae0da560
Ильдар <https://github.com/araev/java-project-61>
2025-10-01 20:27:38 +0700

    Исправлен метод generateNumber после предупреждения Sonar

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -1,9 +1,16 @@
 package hexlet.code;
 
+import java.security.SecureRandom;
+
 public final class Utils {
+    private static final SecureRandom RNG = new SecureRandom();
+
     private Utils() { }
 
     public static int generateNumber(int min, int max) {
-        return (int) Math.floor(Math.random() * (max - min + 1)) + min;
+        if (min > max) {
+            throw new IllegalArgumentException("min > max: " + min + " > " + max);
+        }
+        return RNG.nextInt((max - min) + 1) + min;
     }
 }
